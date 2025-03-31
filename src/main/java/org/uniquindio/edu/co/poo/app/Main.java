4a799217b02e5897705bb2ee1e01e6a264d27784
SebastianEBR <sebastiane.ballesterosr@uqvirtual.edu.co>
2025-03-30 21:57:55 -0500

    Mensaje de bienvenida

--- src/main/java/org/uniquindio/edu/co/poo/app/Main.java
+++ src/main/java/org/uniquindio/edu/co/poo/app/Main.java
@@ -1,7 +1,14 @@
 package org.uniquindio.edu.co.poo.app;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        JOptionPane.showMessageDialog(null, "Hola muchachos como estan??," +
+                "\nla verdad es primera vez que creo un espacio de trabajo pero creo que ya funciona," +
+                "\ncualquier inquietud bienvenida sea," +
+                "\n" +
+                "\ndicho esto, hora de trabajar en este proyecto final");
+
     }
 }
